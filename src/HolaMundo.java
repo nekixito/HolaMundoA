bb6f7ad685e0692ad29e82185c5b1c4dcae08b4c
Miguel Nequiz <nekixito@gmail.com>
2023-09-01 16:08:51 -0600

    Hola Mundo con variables y se agrega el idioma español

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,11 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde Java");
+
+        String saludar = "Hola Mundo desde variable en Java";
+        System.out.println("saludar = " + saludar);
+        System.out.println("saludar en mayúsculas = " + saludar.toUpperCase());
+
+        
     }
 }
