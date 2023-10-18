6ef45b7d2e5ecfd6abe08f84818881cadff6d847
Miguel Nequiz <nekixito@gmail.com>
2023-10-18 16:55:53 -0600

    Sistemas de numeración: decimal, binario, octal y hexadecimal

---
+++ src/SistemasNumericos.java
@@ -0,0 +1,22 @@
+public class SistemasNumericos {
+    public static void main(String[] args) {
+        int numeroDecimal = 30;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        System.out.println("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+
+        int numeroBinario = 0b11110;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        System.out.println("numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+
+        int numeroOctal = 036;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+
+        int numeroHexadecimal = 0x1e;
+        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+
+    }
+}
