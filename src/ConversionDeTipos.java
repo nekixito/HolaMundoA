000c82e37274350f616bc1d80d44890a529ac6cb
Miguel Nequiz <nekixito@gmail.com>
2023-10-27 18:16:56 -0600

    Conversión de tipos primitivos a cadenas

--- src/ConversionDeTipos.java
+++ src/ConversionDeTipos.java
@@ -12,5 +12,24 @@ public class ConversionDeTipos {
         String logicoStr = "true";
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
+
+        int otroNumeroInt = 100;
+
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+        
+        otroNumeroStr = String.valueOf(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        Double otroRealDouble = 12.3456e2;
+        System.out.println("otroNumeroDouble = " + otroRealDouble);
+
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(1.23456e2f);
+        System.out.println("otroRealStr = " + otroRealStr);
     }
 }
